89e6a4769bd4329afb068b815fe5a6f338d7498c
Anthony <lightweightdevelopment@gmail.com>
2015-08-29 22:23:06 +0100

    Bump version

--- test/rick_test.rb
+++ test/rick_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 
 class RickTest < Minitest::Test
   def test_version_number
-    assert_equal Rick::VERSION, "0.1.0"
+    assert_equal Rick::VERSION, "0.1.1"
   end
 
   def test_roll_url
